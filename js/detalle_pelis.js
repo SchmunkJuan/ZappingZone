2efaf9acd71c291e04d6f9a373e3ad9726bebe91
MalenaKrompolcas <mkrompolcas@udesa.edu.ar>
2021-11-23 00:33:12 -0300

    detalle y fav

--- js/detalle_pelis.js
+++ js/detalle_pelis.js
@@ -4,7 +4,7 @@ let qsto = new URLSearchParams(qs); //Transformar la qs en un Objeto Literal
 let id = qsto.get('id'); //Obtener el dato de id del objeto literal
 
 //Armar un nuevo fetch
-let url = (`${id}`)
+let url = `https://api.themoviedb.org/3/movie/${idSerie}?api_key=a070d8766877ff453cfcafc5a8c99cec`
 
 fetch(url)
     .then(function(response){
@@ -16,66 +16,82 @@ fetch(url)
        //Paso 1: capturar DOM.
        let title = document.querySelector('h1');
        let image = document.querySelector('img');
-       let titulo = document.querySelector('.titulo');
-       let calificacion = document.querySelector('.calificacion');
-       let estreno = document.querySelector('.estreno');
-       let duracion = document.querySelector('.duracion');
-       let sinopsis = document.querySelector('.sinopsis');
-       let genero = document.querySelector('.genero');
+       let calificacion = document.querySelector('.calificacionNumero');
+       let estreno = document.querySelector('.fechaNumero');
+       let duracion = document.querySelector('.duracionTexto')
+       let sinopsis = document.querySelector('.sinopsisTexto');
+       let genero = document.querySelector('.generoTexto');
 
        //Paso 2 y 3: actualizar datos y actualizar DOM;
        title.innerText = data.name;
        image.src=data.image;
-       calificacion.innerText += data.calificacion;    
-       estreno.innerText += data.estreno;
-       duracion.innerText += data.duracion;
-       sinopsis.innerText += data.sinopsis;
-       genero.innerText += data.genero;     
 
+       calificacion.innerText = data.vote_average; 
+       duracion.innerText = data.runtime;   
+       estreno.innerText = data.first_air_date;
+       sinopsis.innerText = data.overview;
+
+        let infoGeneros = data.genres;
+        let arrayGeneros = "";
+
+
+        console.log(infoGeneros);
+
+        for(let i = 0; i <infoGeneros.length; i++) {
+            arrayGeneros +=
+            <li>
+                <p><a href="detalle_genero_pelis.html?id=${infoGeneros[i].id}&title=${infoGeneros[i].name}">${infoGeneros[i].name}
+                </a></p>
+            </li>
+        }
+
+        genero.innerHTML = arrayGeneros;
     })
+
     .catch(function(error){
         console.log(error);
-    })
+    });
 
 
     //Crear un array que iremos completando con datos.
-let favoritos = [];
+let seriesFavoritos = [];
 
 //Recuperar storage
-let recuperoStorage = localStorage.getItem('favoritos');
+let recuperoStorage = localStorage.getItem('favoritosPeliculas');
 
 if(recuperoStorage != null){
     //1ro tenemos que transformarlo de cadena de texto y después lo guardamos en favoritos.
-    favoritos = JSON.parse(recuperoStorage);
+    seriesFavoritos = JSON.parse(recuperoStorage);
 }
 
 //Hacer click en el link. Primero debemos capturar el elemnto.
 let fav= document.querySelector('.fav');
 
 //Chequear que el id esté en el array de favoritos
-if(favoritos.includes(id)){
+if(peliculasFavoritos.includes(idPelis)){
     fav.innerText="Quitar de favoritos";
 }
 
 fav.addEventListener('click', function(evento){
     evento.preventDefault();
 
-    if(favoritos.includes(id)){
+    if(peliculasFavoritos.includes(idPelis)) {
         //Si el id está en el array.
-        let indice = favoritos.indexOf(id);
+        let indice = favoritos.indexOf(idPelis);
         //Borrar a partir del índice, un elemento.
-        favoritos.splice(indice, 1)
+        peliculasFavoritos.splice(indice, 1)
         fav.innerText="Agregar a favoritos"
     } else {
          //Agregar un dato al array.
-        favoritos.push(id);
+        peliculasFavoritos.push(idPelis);
         fav.innerText="Quitar de favoritos";
     }
  
+    console.log(localStorage
+        )
     //Guardar el array en el storage
-    let favsToString = JSON.stringify(favoritos); //Transformamos el array en una cadena de texto.
-    localStorage.setItem('favoritos', favsToString); //Guardamos el array en el storage.
+    let favsToString = JSON.stringify(peliculasFavoritos); //Transformamos el array en una cadena de texto.
+    localStorage.setItem('favoritosPeliculas', favsToString); //Guardamos el array en el storage.
 
-    console.log(localStorage);
 
 })
--- js/favorite.js
+++ js/favorite.js
@@ -1,52 +1,123 @@
-console.log('favoritos');
+console.log('favoritosPeliculas');
+console.log('favoritosSerie')
 //Recupero storage
-let recuperoStorage = localStorage.getItem('favoritos');
+let favoritosPelisStorage = localStorage.getItem("favoritosPeliculas");
+let favoritosSerieStorage = localStorage.getItem("favoritosSerie");
 //y transformar de json en array
-let favoritos = JSON.parse(recuperoStorage);
-console.log(favoritos); 
+let favoritosPeliculas = JSON.parse(favoritosPeliculasStorage);
+let favoritosSerie = JSON.parse(favoritosSerieStorage);
 
+console.log(favoritosPeliculas); 
+console.log(favoritosSerie); 
 
 //Capturar el contenedor de los elementos a mostar
 let section = document.querySelector('.lista');
-let personajesFavoritos = '';
-
 
 //Si el storage está vacío indicamos al usuario que no hay favoritos seleccionados.
-if(favoritos == null || favoritos.length == 0){
+if(favoritosPeliculas == null || !favoritosPeliculas || favoritosPeliculas.length) {
     section.innerHTML='<p>No hay favoritos seleccionados</p>'
 } else {
-    //for para recorrer el array.
-    for (let i=0; i<favoritos.length; i++){
-        buscarYMostarFavoritos(favoritos[i]);
+    if (favoritosPeliculas != null | favoritosPeliculas) {
+        favoritosPeliculas.forEach((id) => {
+            console.log(id);
+            buscarYMostarFavoritosPeliculas(id)
+        });
+    }
+    
+    function buscarYMostrarFavoritosPeliculas(id) {
+        fetch(`https://api.themoviedb.org/3/movie/${idSerie}?api_key=a070d8766877ff453cfcafc5a8c99cec`)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            if (data) {
+              document.querySelector(".tapas").innerHTML += `
+            <li>  
+              <a href="detalle_pelis.html?id=${id}">
+                <img src=''>
+                <p>${data.title}</p>
+              </a>
+            </li>`;
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });    
     }
 
+    
 }
-
-
-function buscarYMostarFavoritos(id){
-    //fetch para buscar cada elemento del array. 
-    let url = `https://api.themoviedb.org/3/movie/550?api_key=dc595f48c885d38c5c9265bb69a0626c=${id}`
-
-    fetch(url)
-        .then( function(response){
+if(favoritosSerie == null || !favoritosSerie || favoritosSerie.length) {
+    section.innerHTML='<p>No hay favoritos seleccionados</p>'
+} else {
+    if (favoritosSerie != null | favoritosSerie) {
+        favoritosSerie.forEach((id) => {
+            console.log(id);
+            buscarYMostarFavoritosSerie(id)
+        });
+    }
+    function buscarYMostrarFavoritosSerie(id) {
+        fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=7c98c63c7f5b48716db97eeade9c8a32&language=en-US`)
+            .then(function (response) {
             return response.json();
-        })
-        .then( function(data){
-            console.log(data);
-            personajesFavoritos += `<article>
-                                <img src=${data.image}>
-                                <p>Nombre: ${data.name}</p>
-                                <p>Status: ${data.status}</p>
-                                <a href="detalle.html?id=${data.id}">Ver más</a>
-                            </article>`
-        
-            //mostarlo al usuario.
-            section.innerHTML = personajesFavoritos;            
-        })
-        .catch( function(error){
+            })
+            .then(function (data) {
+            if (data) {
+                console.log(data.title);
+                document.querySelector(".tapas").innerHTML += `
+                <li>  
+                    <a href="detalle_serie.html?id=${id}">
+                        <img src=''>
+                        <p>${data.name}</p>
+                    </a>
+                </li>`;
+                }
+            })
+            .catch(function (error) {
             console.log(error);
-        })
-
+        });
+    }
 }
- 
 
+//Boton de Favoritos
+
+//Creo array a rellenar de peliculas favoritas
+let peliculasFav = []
+
+//Recupero el storage
+let recuperoStorage = localStorage.getItem('peliculasFav');
+
+//Reviso si el id ya esta en favoritos
+if (recuperoStorage != null){ //Sucede si hay datos en el storage
+    peliculasFav = JSON.parse(recuperoStorage);
+}
+
+//Capturo el elemento en el DOM
+let fav = document.querySelector('.favoritos');
+let botonFav = document.querySelector('.botonFav')
+
+if(peliculasFav.includes(id)){
+    botonFav.innerText = 'Quitar de Favoritos'
+}
+
+fav.addEventListener('click', function(evento){
+    evento.preventDefault();
+
+    //En el caso de que la pelicula ya este en favoritos y el usuario quiera sacarla
+    if (peliculasFav.includes(id)){
+        let indice = peliculasFav.indexOf(id);
+        peliculasFav.splice(indice, 1);
+        botonFav.innerText = 'Agregar a Favoritos'
+
+    } else {
+    //En el caso de que la pelicula no este en favoritos
+        peliculasFav.push(id);
+        botonFav.innerHTML = 'Quitar de Favoritos';
+    }
+    console.log(peliculasFav);
+    //Guardo el array en el storage
+    let favToStirng = JSON.stringify(peliculasFav); //Se transforma al array en una cadena de texto
+
+    localStorage.setItem('peliculasFav', favToStirng) //Guardo la info en el storage
+    }
+})
